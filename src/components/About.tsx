59d4ca9a742769e2de8e72aa13b18b67275fc728
0Aryan1 <aryanagrawal0102@gmail.com>
2026-03-09 01:49:59 +0530

    change in about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,13 +7,15 @@ const About = () => {
         <h3 className="title">About Me</h3>
         <p className="para">
           Hello! I'm Aryan Agrawal, from Firozabad, the city of Bangles.
-
-I am a creative, goal-oriented individual with strong moral values, excellent communication skills, organizational abilities, and meticulous attention to detail.
-
-I am currently pursuing a Bachelor's of Engineering at JIIT Noida (2022–2026).
-
-I have been the Public Relation Head of Parola Society - JIIT and the Marketing Head of , the CICE Hub.
-
+          <br />
+          <br />
+          I am a creative, goal-oriented individual with strong moral values, excellent communication skills, organizational abilities, and meticulous attention to detail.
+          <br />
+          <br />
+          I am currently pursuing a Bachelor's of Engineering at JIIT Noida (2022–2026).
+          <br />
+          <br />
+          I have been the Public Relation Head of Parola Society - JIIT and the Marketing Head of the CICE Hub.
         </p>
       </div>
     </div>
